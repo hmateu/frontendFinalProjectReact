b29556a1e71a337f0ea0988228f538bbc34c765d
hmateu <hmateu.ortola@gmail.com>
2023-07-19 21:36:00 +0200

    style: Improves the design of the buttons on the home page

--- src/pages/Body/Body.jsx
+++ src/pages/Body/Body.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { Home } from "../Home/Home";
-import { Login } from "../Login.jsx/Login";
+import { Login } from "../Login/Login";
 import { Register } from "../Register/Register";
 
 export const Body = () => {
